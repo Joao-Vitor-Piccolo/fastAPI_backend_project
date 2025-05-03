5baf68965ebdc8cb1c044baa32fd480731f48e24
joaovitorpiccolo <joaovitorpiccolo015@gmail.com>
2025-05-03 16:04:23 -0300

    Making client-side test to verify the user post request

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,8 +2,7 @@ from fastapi.testclient import TestClient
 from src.fastapi_course.app import app  # Importamos desta forma pois o import é realizado na raiz do projeto.
 from http import HTTPStatus
 
-
-## client = TestClient(app)  # Colocamos aqui o objeto app, no caso o app.py que criamos na main.
+client = TestClient(app)  # Colocamos aqui o objeto app, no caso o app.py que criamos na main.
 
 
 # Ou seja, o objeto que queremos testar
@@ -23,17 +22,13 @@ def test_read_root():  # "Deve retornar OK e Hello World!"
 def test_create_user():
     client = TestClient(app)  # Arrange (Organização do teste)
 
-    headers = {
-        "username": "João",
-        "email": "joao@example.com",
-        "password": "renatinho123"
-    }
+    json = {'username': 'testusername', 'password': 'passwordtest12', 'email': 'emaildozé@emaildoze.com'}
 
-    response = client.post('/users/', json=headers)  # Act (ação)
+    response = client.post('/users/', json=json)
 
-    assert response.status_code == HTTPStatus.OK
+    assert response.status_code == HTTPStatus.CREATED
 
-    assert response.json() == {'message': 'User Created'}
+    assert response.json() == {'username': 'testusername', 'email': 'emaildozé@emaildoze.com', 'id': 1}
 
 
 def test_read_user():
