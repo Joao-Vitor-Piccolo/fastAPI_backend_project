fa41fe360858ca2e536192ac3ac5e69180bbcf57
joaovitorpiccolo <joaovitorpiccolo015@gmail.com>
2025-06-02 10:44:01 -0300

    Commiting sign in static files p/4

--- src/static_files/src/js/script_post_login.js
+++ src/static_files/src/js/script_post_login.js
@@ -1,15 +1,17 @@
 window.addEventListener('scroll', function() {
   const navbar = document.getElementById('navbar');
-  const navcenter = document.getElementById('nav-center');
+  const logo = document.getElementById('logo');
 
   const scrollLimit = window.innerHeight * 0.8;
 
   if (window.scrollY >= scrollLimit) {
     navbar.classList.add('scrolled');
-    nav-center.classList.add('scrolled');
+    logo.classList.add('scrolled');
+    logo.src='../src/assets/imgs/sp_icon_preta.png'
   } else {
     navbar.classList.remove('scrolled');
-    navcenter.classList.remove('scrolled');
+    logo.classList.remove('scrolled');
+    logo.src = "../src/assets/imgs/"
   }
 });
 
